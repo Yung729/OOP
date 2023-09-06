9b7148b31ea9826333b817c29ca01af99e5b8b8d
Yung729 <leongkahyung@gmail.com>
2023-09-06 19:17:28 +0800

    Update Assignment.java

--- src/Assignment/Assignment.java
+++ src/Assignment/Assignment.java
@@ -156,6 +156,8 @@ public class Assignment {
                 
                 case 2 ->{Stock.stockAvailableMenu();}
                 
+                case 4 ->{new Member().memberMenu();}
+                
                 case 5 ->{stockMainMenu();}
             
                 case 7 -> {}
