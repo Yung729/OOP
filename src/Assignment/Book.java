1d4c1a81f04fd8d765046d3fea8b428a070fb70e
Yung729 <leongkahyung@gmail.com>
2023-09-21 11:51:14 +0800

    m

--- src/Assignment/Book.java
+++ src/Assignment/Book.java
@@ -12,7 +12,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import static java.lang.Character.toUpperCase;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
